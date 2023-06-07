952c25d18917c506a17b3e687b0842947b8fc864
gaurav876409 <gaurav876409@gmail.com>
2023-06-07 07:55:52 +0530

    update

--- src/components/App.js
+++ src/components/App.js
@@ -1,3 +1,55 @@
+// import React, { useState, useEffect } from 'react';
+// import '../styles/App.css';
+// import { Loader } from './Loader';
+// import { PhotoFrame } from './PhotoFrame';
+
+// const App = () => {
+//   const [photoId, setPhotoId] = useState('');
+//   const [loading, setLoading] = useState(false);
+//   const [photoData, setPhotoData] = useState(null);
+
+//   useEffect(() => {
+//     if (photoId) {
+//       setLoading(true);
+//       fetch(`https://jsonplaceholder.typicode.com/photos/${photoId}`)
+//         .then((response) => response.json())
+//         .then((data) => {
+//           setPhotoData(data);
+//           setLoading(false);
+//         })
+//         .catch((error) => {
+//           console.error('Error fetching photo data:', error);
+//           setLoading(false);
+//         });
+//     }
+//   }, [photoId]);
+
+//   const handlePhotoIdChange = (event) => {
+//     setPhotoId(event.target.value);
+//   };
+
+//   return (
+//     <div id="main">
+//       <div className="input-container">
+//         <label htmlFor="photo-id-input">Id number</label>
+//         <input
+//           id="photo-id-input"
+//           type="number"
+//           value={photoId}
+//           onChange={handlePhotoIdChange}
+//         />
+//       </div>
+//       {loading ? (
+//         <Loader />
+//       ) : photoData ? (
+//         <PhotoFrame url={photoData.url} title={photoData.title} />
+//       ) : null}
+//     </div>
+//   );
+// };
+
+// export default App;
+
 import React, { useState, useEffect } from 'react';
 import '../styles/App.css';
 import { Loader } from './Loader';
@@ -11,6 +63,10 @@ const App = () => {
   useEffect(() => {
     if (photoId) {
       setLoading(true);
+
+      // Reset photoData while loading
+      setPhotoData(null);
+
       fetch(`https://jsonplaceholder.typicode.com/photos/${photoId}`)
         .then((response) => response.json())
         .then((data) => {
@@ -49,3 +105,4 @@ const App = () => {
 };
 
 export default App;
+
